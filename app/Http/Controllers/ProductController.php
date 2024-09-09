653b05ea2915d1844d2ac7aa1b9f0ec2282bff8b
Morteza Pouretemadi <m.pouretemadi@digikala.com>
2024-09-09 17:42:04 +0330

    fix discount rule product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -205,7 +205,7 @@ class ProductController extends Controller
             "inventory" => "required|integer",
             "discount_percent" => "nullable|integer",
             "special_offer" => "boolean",
-            "discount_rules" => "required|array",
+            "discount_rules" => "required|string",
             "description" => "required|string",
             "technical_description" => "nullable|string",
             "faq" => "nullable|string",
@@ -215,6 +215,13 @@ class ProductController extends Controller
 
         $product = Product::findOrFail($id);
         $data = $request->all();
+        try {
+            json_decode($data['discount_rules']);
+        } catch (\Throwable $e) {
+            logger()->error($e);
+            return response()->json('Discount rules not valid', 400);
+        }
+
         $data["description"] = HtmlPurifierHelper::clean($request->input("description"));
         $data["technical_description"] = HtmlPurifierHelper::clean($request->input("technical_description"));
 
